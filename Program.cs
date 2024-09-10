b0c80afc2800926425d65cd9afd9320a46f45645
jcampeau25 <jdccanuck25@gmail.com>
2024-09-10 09:35:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Topic_3___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomNumber, gradYear;
+            double price;
+            string course, name;
+
+            roomNumber = 29;
+            price = 2.99;
+            course = "Computer Science";
+            name = "Josh Campeau";
+            gradYear = 2026;
+
+            Console.WriteLine("This is room # " + roomNumber);
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine("I am learning " + course);
+            Console.WriteLine();   
+            Console.WriteLine("My name is " + name + " and I graduate in " + gradYear);
+
+        }
+    }
+}
